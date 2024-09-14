d061d5787dc45f190d47c70b553a706f6a4eaac3
Darwin Zamarca G <dzamarca@subsole.com>
2024-09-14 01:47:55 -0300

    add - carga inicial

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// Desafio01
+let estadoImgDesafio01 = 0
+imgDesafio01 = document.querySelector(".imgDesafio01")
+
+//Desafio02
+let totalDesafio02 = 0
+let msjResultadoDesafio02 = ""
+let resultadoDesafio02 = document.querySelector(".resultadoDesafio02")
+
+//Desafio03
+let keyDesafio03 = ""
+let msjResultadoDesafio03 = ""
+let resultadoDesafio03 = document.querySelector(".resultadoDesafio03")
